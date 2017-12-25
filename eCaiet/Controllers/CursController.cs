68c8e23a1d765928db6726d3b1b5ae892c40d4a3
PiKarelia <ana.grigorcea@yahoo.com>
2017-12-25 18:52:41 +0200

    some shit with nav side bar, added to ignore

---
+++ eCaiet/Controllers/CursController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eCaiet.Controllers
+{
+    public class CursController : Controller
+    {
+        // GET: Curs
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
